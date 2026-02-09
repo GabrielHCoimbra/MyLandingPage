f95420dce48dffc9d8bc34557bcd651349d74713
GabrielHCoimbra <gabriel.coimbra.viana@gmail.com>
2026-02-09 10:44:15 +0000

    OldMatrix only contented in the section

---
+++ my-landing-page/src/components/Section/section.jsx
@@ -0,0 +1,21 @@
+import './Section.css'
+import MatrixBackground from '../MatrixBackground/matrixbackground.jsx'
+
+
+export default function Section() {
+return (
+    <>
+        <section id='initial'>
+            <MatrixBackground />
+            <h1>Gabriel Viana</h1>
+            <h2>Software Engineer</h2>
+        </section>
+       
+        <section id="about">
+            <h2>Sobre mim</h2>
+            <p>Conteúdo conforme README</p>
+        </section>
+        
+    </>
+)
+}
--- my-landing-page/src/pages/home.jsx
+++ my-landing-page/src/pages/home.jsx
@@ -2,27 +2,18 @@
 // Estrutura principal da página inicial
 
 
-import MatrixBackground from '../components/MatrixBackground/MatrixBackground'
+
 import Navbar from '../components/Navbar/Navbar'
+import Section from '../components/Section/Section'
 
 
 export default function Home() {
 return (
 <>
-<MatrixBackground />
 <Navbar />
+<Section />
 
 
-<section id="hero">
-<h1>Gabriel Viana</h1>
-<h2>Software Engineer</h2>
-</section>
-
-
-<section id="about">
-<h2>Sobre mim</h2>
-<p>Conteúdo conforme README</p>
-</section>
 </>
 )
 }
